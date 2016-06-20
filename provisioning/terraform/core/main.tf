6772e9e9a252fe31efb9bd9e6114b49dc90c7504
Phivos Stylianides <stphivos@gmail.com>
2016-06-20 16:05:30 +0200

    Include core tfvars in deploy rds

--- provisioning/terraform/core/main.tf
+++ provisioning/terraform/core/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 # Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
 # Create an internet gateway to give our subnet access to the outside world
